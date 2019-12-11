f82339fb821826073cf1f548974fd81a1a0b9d70
numenoreon <numenoreon@gmail.com>
2019-12-11 13:31:15 +0100

    Clean

--- CodilityCpp/Lesson6/MaxProductOfThree.cpp
+++
@@ -1,8 +0,0 @@
-#include <algorithm>
-
-int solution(vector<int> &A) {
-
-    sort(begin(A), end(A));
-    int N=A.size();
-    return A[N-1]*A[N-2]*A[N-3] > A[N-1]*A[0]*A[1] ? A[N-1]*A[N-2]*A[N-3] : A[N-1]*A[0]*A[1];
-}
