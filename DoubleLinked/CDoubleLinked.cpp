faf27afba7cdae31a3490dbdf02f64e655a1fadc
numenoreon <54109926+numenoreon@users.noreply.github.com>
2019-12-16 20:00:22 +0100

    Update CDoubleLinked.cpp

--- DoubleLinked/CDoubleLinked.cpp
+++ DoubleLinked/CDoubleLinked.cpp
@@ -1,12 +1,11 @@
 #include "CDoubleLinked.h"
 #include <iostream>
 
-CDoubleLinked::CDoubleLinked() {
+CDoubleLinked::CDoubleLinked() : sz(0) {
 	head = tail = nullptr;
 }
 
-CDoubleLinked::CDoubleLinked(int d)
-{
+CDoubleLinked::CDoubleLinked(int d) : sz(1) {
 	head = new CNode(d);
 	tail = head;
 }
@@ -46,21 +45,32 @@ void CDoubleLinked::addFront(int d) {
 	if (!head){
 		head = new CNode(d);
 		tail = head;
+		++sz;
 	}
 	else {
 		CNode* temp = new CNode(d);
 		temp->setNext(head);
 		head->setPrev(temp);
 		head = temp;
+		++sz;
 	}
 }
 
 void CDoubleLinked::deleteFront() {
-	if (head) {
+	
+	if (head == tail && head!=nullptr)
+	{
+		CNode* temp = head;
+		delete temp;
+		head = tail = nullptr;
+		--sz;
+	}
+	else if (head) {
 		CNode* temp = head;
 		head = head->giveNext();
 		head->setPrev(nullptr);
 		delete temp;
+		--sz;
 	}
 }
 
@@ -68,20 +78,68 @@ void CDoubleLinked::addBack(int d) {
 	if (!head) {
 		head = new CNode(d);
 		tail = head;
+		++sz;
 	}
 	else {
 		CNode* temp = new CNode(d);
 		temp->setPrev(tail);
 		tail->setNext(temp);
 		tail = temp;
+		++sz;
 	}
 }
 
 void CDoubleLinked::deleteBack() {
-	if (head) {
+	if (head == tail && head != nullptr)
+	{
+		CNode* temp = head;
+		delete temp;
+		head = tail = nullptr;
+		--sz;
+	}
+	else if (head) {
 		CNode* temp = tail;
 		tail = tail->givePrev();
 		tail->setNext(nullptr);
 		delete temp;
+		--sz;
+	}
+}
+
+void CDoubleLinked::addData(int d, int p) {
+	if (p >= sz) {
+		addBack(d);
+	}
+	else {
+		CNode* temp = head;
+		while (--p) {
+			temp = temp->giveNext();
+		}
+		CNode* temp2 = temp->giveNext();
+		temp->setNext(new CNode(d));
+		temp->giveNext()->setPrev(temp);
+		temp->giveNext()->setNext(temp2);
+		temp2->setPrev(temp->giveNext());
+	}
+}
+
+void CDoubleLinked::deleteData(int d) {
+	if (head) {
+		if (head->get() == d) {
+			deleteFront();
+		}
+		else {
+			CNode* temp = head;
+			while (temp->get() != d)
+			{
+				temp = temp->giveNext();
+				if (temp==nullptr) break;
+			}
+			if (temp!=nullptr) {
+				CNode* temp2 = temp;
+				temp->givePrev()->setNext(temp->giveNext());
+				temp->giveNext()->setPrev(temp->givePrev());
+			}
+		}
 	}
 }
