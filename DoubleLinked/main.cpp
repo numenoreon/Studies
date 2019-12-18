2a89caf0002ec00cfd63cb8747b10e85f4d9f6a7
numenoreon <54109926+numenoreon@users.noreply.github.com>
2019-12-18 17:15:42 +0100

    Update main.cpp

--- DoubleLinked/main.cpp
+++ DoubleLinked/main.cpp
@@ -1,29 +1,11 @@
 #include "CDoubleLinked.h"
+#include "CPerson.h"
 
 int main()
 {
-	CDoubleLinked dL;
-	dL.addFront(1);
-	dL.deleteBack();
-	dL.addBack(4);
-	dL.addBack(43);
-	dL.addFront(21);
-	dL.addBack(14);
-	dL.addFront(15);
+	CDoubleLinked <CPerson> dL;
+	dL.addBack(CPerson("One", "Two", 69));
+	dL.addBack(CPerson("Three", "Six", 1));
 	dL.showFront();
-	dL.addData(69, 3);
-	dL.showFront();
-	dL.deleteBack();
-	dL.addBack(56);
-	dL.addBack(11);
-	dL.addFront(90);
-	dL.showFront();
-	dL.deleteData(21);
-	dL.showFront();
-	dL.addData(77, 3);
-	dL.addBack(104);
-	dL.addFront(81);
-	dL.showFront();
-	dL.deleteData(1001);
 	return 0;
 }
