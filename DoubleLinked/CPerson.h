7909271eb7b7284f1a68e0c4ffb15e0755e76534
numenoreon <54109926+numenoreon@users.noreply.github.com>
2019-12-19 22:13:53 +0100

    Update CPerson.h

--- DoubleLinked/CPerson.h
+++ DoubleLinked/CPerson.h
@@ -10,10 +10,10 @@ public:
 	CPerson();
 	CPerson(std::string fN, std::string sN, double a);
 
-	friend std::ostream& operator<<(std::ostream& out, const CPerson& temp);
-};
-
-std::ostream& operator<<(std::ostream& out, const CPerson& temp) {
+	friend std::ostream& operator<<(std::ostream& out, const CPerson& temp) {
 	out << temp.fName << " " << temp.sName << " " << temp.account;
 	return out;
-}
+	}
+};
+
+
